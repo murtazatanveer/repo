71055dcd8e13335b52af6d1f1b927eec5849986e
Murtaza_Tanveer <murtaza.tanveer488@gmail.com>
2024-06-01 01:55:30 +0500

    LeetCode Problems.

--- Linear_Search.java
+++ Linear_Search.java
@@ -676,6 +676,153 @@ private boolean contains(int []arr,int check){
 //     return min;
 // }
 
+
+/* 1. Two Sum
+Easy
+Topics
+Companies
+Hint
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+ 
+
+Constraints:
+
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109 */
+
+int[] twoSum(int[] nums, int target) {
+        
+    for (int i = 0; i < nums.length-1; i++){
+
+        for (int j = i+1; j < nums.length; j++) {
+
+            if(nums[i]+nums[j]==target){ return new int[]{i,j};}
+            
+        }
+        
+    }
+
+    return new int[]{-1,-1};
+    
+}
+
+/*  167. Two Sum II - Input Array Is Sorted
+
+Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+
+Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+
+The tests are generated such that there is exactly one solution. You may not use the same element twice.
+
+Your solution must use only constant extra space.
+
+ 
+
+Example 1:
+
+Input: numbers = [2,7,11,15], target = 9
+Output: [1,2]
+Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return [1, 2].
+Example 2:
+
+Input: numbers = [2,3,4], target = 6
+Output: [1,3]
+Explanation: The sum of 2 and 4 is 6. Therefore index1 = 1, index2 = 3. We return [1, 3].
+Example 3:
+
+Input: numbers = [-1,0], target = -1
+Output: [1,2]
+Explanation: The sum of -1 and 0 is -1. Therefore index1 = 1, index2 = 2. We return [1, 2].
+ 
+
+Constraints:
+
+2 <= numbers.length <= 3 * 104
+-1000 <= numbers[i] <= 1000
+numbers is sorted in non-decreasing order.
+-1000 <= target <= 1000
+The tests are generated such that there is exactly one solution. */
+
+public int[] twoSum2(int[] numbers, int target) {
+
+    for (int i = 0; i < numbers.length-1; i++){
+
+        for (int j = i+1; j < numbers.length && numbers[i]+numbers[j]<=target ; j++) {
+
+            if(numbers[i]+numbers[j]==target){ return new int[]{i,j};}
+            
+        }
+        
+    }
+
+    return new int[]{-1,-1}; 
+}
+
+}
+
+/* 520. Detect Capital
+
+We define the usage of capitals in a word to be right when one of the following cases holds:
+
+All letters in this word are capitals, like "USA".
+All letters in this word are not capitals, like "leetcode".
+Only the first letter in this word is capital, like "Google".
+Given a string word, return true if the usage of capitals in it is right.
+
+ 
+
+Example 1:
+
+Input: word = "USA"
+Output: true
+Example 2:
+
+Input: word = "FlaG"
+Output: false
+ 
+
+Constraints:
+
+1 <= word.length <= 100
+word consists of lowercase and uppercase English letters.*/
+
+
+public boolean detectCapitalUse(String word) {
+
+    int count=0;
+    
+    for (int i = 0; i < word.length(); i++) {
+        if(word.charAt(i)>=65 && word.charAt(i)<=90){
+            count++;}
+    }
+
+    if(count==word.length() || count==0){return true;}
+
+    if(word.charAt(0)>=65 && word.charAt(0)<=95 && count==1){return true;}
+
+    return false;
+    
 }
 
 
