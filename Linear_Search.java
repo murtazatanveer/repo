4e3c5e16c8c6c771ac4e4f85d4aa3b7a45854dce
Murtaza_Tanveer <murtaza.tanveer488@gmail.com>
2024-06-03 01:58:25 +0500

    Leet Code Problems.

--- Linear_Search.java
+++ Linear_Search.java
@@ -779,7 +779,7 @@ public int[] twoSum2(int[] numbers, int target) {
     return new int[]{-1,-1}; 
 }
 
-}
+
 
 /* 520. Detect Capital
 
@@ -823,6 +823,107 @@ public boolean detectCapitalUse(String word) {
 
     return false;
     
+}
+
+
+/* Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+A word is a maximal 
+substring
+ consisting of non-space characters only.
+
+ 
+
+Example 1:
+
+Input: s = "Hello World"
+Output: 5
+Explanation: The last word is "World" with length 5.
+Example 2:
+
+Input: s = "   fly me   to   the moon  "
+Output: 4
+Explanation: The last word is "moon" with length 4.
+Example 3:
+
+Input: s = "luffy is still joyboy"
+Output: 6
+Explanation: The last word is "joyboy" with length 6.
+ 
+
+Constraints:
+
+1 <= s.length <= 104
+s consists of only English letters and spaces ' '.
+There will be at least one word in s */
+
+public int lengthOfLastWord(String s) {
+
+    int i = s.length()-1;
+    int count=0;
+  while(s.charAt(i)==32){ i--;}
+
+  
+  for (int j = i; j>=0&&s.charAt(j)!=32; j--) {
+    count++;
+  }
+    
+return count;
+        
+}
+
+/* 66. Plus One
+Easy
+Topics
+Companies
+You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+
+Increment the large integer by one and return the resulting array of digits.
+
+ 
+
+Example 1:
+
+Input: digits = [1,2,3]
+Output: [1,2,4]
+Explanation: The array represents the integer 123.
+Incrementing by one gives 123 + 1 = 124.
+Thus, the result should be [1,2,4].
+Example 2:
+
+Input: digits = [4,3,2,1]
+Output: [4,3,2,2]
+Explanation: The array represents the integer 4321.
+Incrementing by one gives 4321 + 1 = 4322.
+Thus, the result should be [4,3,2,2].
+Example 3:
+
+Input: digits = [9]
+Output: [1,0]
+Explanation: The array represents the integer 9.
+Incrementing by one gives 9 + 1 = 10.
+Thus, the result should be [1,0].*/
+
+public int[] plusOne(int[] digits) {
+
+
+   for (int i = digits.length-1; i >= 0; i--) {
+    if(digits[i]<9){
+        digits[i]++;
+        return digits;
+    }
+    digits[i]=0;
+   }
+    digits=new int[digits.length+1];
+    digits[0]=1;
+    for(int i=1;i<digits.length;i++){
+        digits[i]=0;
+    }
+    
+    return digits;
+    
+}
+
 }
 
 
