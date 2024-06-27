888a44517daf5f00c89f17c6b5e711320b2b8abd
Murtaza_Tanveer <murtaza.tanveer488@gmail.com>
2024-06-27 11:23:08 +0500

    Binary Search Practice.

--- Binary_Search/Binary_Search.java
+++ Binary_Search/Binary_Search.java
@@ -4,8 +4,10 @@ class binarySearch{
 
     // Binary Search for both Assending and Decending order.
 
-     int binary_search(int []arr,int target,int start,int end){
+     int binary_search(int []arr,int target){
 
+        int start = 0;
+        int end = arr.length-1;
         int mid;
        
         boolean isAssen = arr[start]<=arr[end];
@@ -42,10 +44,8 @@ class binarySearch{
 
     // return the index of Target Element But the array is in Assending Order.
 
-    int searchInAssendingArray(int[] nums, int target){
+    int searchInAssendingArray(int[] nums , int target , int start , int end){
 
-        int start=0;    
-        int end = nums.length-1;
         int mid;
 
         while (start<=end) {
@@ -707,13 +707,102 @@ nums is an ascending array that is possibly rotated.
 
 int search(int[] nums, int target){
     
+if(nums.length==1 || nums.length==2){
 
-    
+    for (int i = 0; i < nums.length; i++) {
+        if(nums[i]==target){return i;}
+    }
     return -1;
 }
 
+int pivot = pivotElementIndex(nums);
+
+if(pivot==-1){
+    return searchInAssendingArray(nums, target, 0, nums.length-1);
 }
 
+else if(target>nums[0]){
+    return searchInAssendingArray(nums, target, 0,pivot);
+}
+ else if(target<nums[0]){
+    return searchInAssendingArray(nums, target, pivot+1,nums.length-1);
+ 
+ }else{
+    return pivot;
+ }
+    
+    
+}
+
+private int pivotElementIndex(int []arr){
+
+    int start=0;
+    int end=arr.length-1;
+    int mid=-1;
+
+    while (start<=end){
+
+        mid = start + ((end-start)/2);
+
+        if(arr[mid]>arr[mid+1] && arr[mid]>arr[mid-1]){
+            return mid;
+        }
+        else if(arr[mid]>arr[start]){
+            start=mid+1;
+        }else{
+            end=mid-1;
+        }
+        
+    }
+
+    return -1;
+
+}
+
+
+/* 74. Search a 2D Matrix
+
+You are given an m x n integer matrix matrix with the following two properties:
+
+Each row is sorted in non-decreasing order.
+The first integer of each row is greater than the last integer of the previous row.
+Given an integer target, return true if target is in matrix or false otherwise.
+
+You must write a solution in O(log(m * n)) time complexity.
+
+ 
+
+Example 1:
+
+
+Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+Output: true
+Example 2:
+
+
+Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
+Output: false
+ 
+
+Constraints:
+
+m == matrix.length
+n == matrix[i].length
+1 <= m, n <= 100
+-104 <= matrix[i][j], target <= 104 */
+
+boolean searchMatrix(int[][] matrix, int target) {
+
+    for (int[] arr : matrix) {
+        if(isElementExist(arr, target)){
+        return true;
+       }
+    }
+    return false;
+
+}
+
+}
 
 
 public class Binary_Search {
